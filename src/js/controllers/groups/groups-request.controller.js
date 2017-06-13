5fa97c8a1dc561529ec1f0bd89140492267b2a5f
laseq <lourenco.sequeira@outlook.com>
2017-06-13 09:41:23 +0100

    Clearing unused code

--- src/js/controllers/groups/groups-request.controller.js
+++ src/js/controllers/groups/groups-request.controller.js
@@ -14,25 +14,6 @@ function GroupsRequestCtrl(group, User, Request, $uibModalInstance, $state) {
   vm.usersToInvite = [];
   let massRequest = [];
 
-  // function searchForUserByEmail() {
-  //   console.log('vm.currentEmail:', vm.currentEmail);
-  //   // const userObj = {
-  //   //   email: vm.currentEmail
-  //   // };
-  //   // console.log('userObj:', userObj);
-  //   User
-  //     .findByEmail({ email: vm.currentEmail })
-  //     .$promise
-  //     .then(user => {
-  //       vm.usersToInvite.push(user);
-  //       console.log('vm.usersToInvite:', vm.usersToInvite);
-  //       vm.currentEmail = '';
-  //     })
-  //     .catch(err => {
-  //       console.log('User wasn\'t found');
-  //       console.log('error:', err);
-  //     });
-  // }
   function searchForUserByEmail() {
     const userObj = {
       email: vm.currentEmail,
